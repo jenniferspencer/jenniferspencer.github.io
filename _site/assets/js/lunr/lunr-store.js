ef9d6fa57923404a81412a8468a045d8fc641ac5
Jenny <53540484+jenniferspencer@users.noreply.github.com>
2026-07-24 22:43:22 -0500

    tried fixing permalink

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,19 @@
-var store = []
+var store = [{
+        "title": "About",
+        "excerpt":"A link to a CV  ","categories": [],
+        "tags": [],
+        "url": "/CV/",
+        "teaser": null
+      },{
+        "title": "About",
+        "excerpt":"  Guide to Papers by Topic:     Cancers Included: Cervical 🟦 | Colorectal 🟧 | Breast 🟪     Methods Incorporated: Simulation Modeling 🔲 | Health Equity ❇️ 1.     Spencer JC, Montealegre JR, Calderón-Mora J, Pignone MP. Evaluating Breast, Cervical and Colorectal Cancer Screening Rates in Women 45 to 64. JAMA Netw Open. 2026...","categories": [],
+        "tags": [],
+        "url": "/Publications/",
+        "teaser": null
+      },{
+        "title": "About",
+        "excerpt":"My work relies on econometric and decision science methods to address issues of health equity in the topic of cancer screening and prevention. A few of my ongoing research projects include: Understanding and modeling differences in breast and cervical cancer screening by sexual orientation Modeling approaches to improve Colorectal Cancer...","categories": [],
+        "tags": [],
+        "url": "/about/",
+        "teaser": null
+      },]
