61bf2f0ad7fd0d1264d2f6ecc25ce0f8849dc758
Jenny <53540484+jenniferspencer@users.noreply.github.com>
2026-07-25 19:59:18 -0500

    Capitalization
    Deleted unneeded files and fixed the capitalization

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,4 +1,10 @@
 var store = [{
+        "title": "About",
+        "excerpt":"My work relies on econometric and decision science methods to address issues of health equity in the topic of cancer screening and prevention. A few of my ongoing research projects include: Understanding and modeling differences in breast and cervical cancer screening by sexual orientation Modeling approaches to improve Colorectal Cancer...","categories": [],
+        "tags": [],
+        "url": "/about/",
+        "teaser": null
+      },{
         "title": "CV",
         "excerpt":"A link to a CV  ","categories": [],
         "tags": [],
@@ -9,11 +15,5 @@ var store = [{
         "excerpt":"       Legend:     Cancers Included:             Cervical 🟦 | Colorectal 🟧 | Breast 🟪     Methods Incorporated:             Simulation Modeling 🔲 | Health Equity ❇️ Publications 1.     Spencer JC, Montealegre JR, Calderón-Mora J, Pignone MP. Evaluating Breast, Cervical and Colorectal Cancer Screening Rates in Women 45 to 64. JAMA Netw Open. 2026 May 01;...","categories": [],
         "tags": [],
         "url": "/Publications/",
-        "teaser": null
-      },{
-        "title": "About",
-        "excerpt":"My work relies on econometric and decision science methods to address issues of health equity in the topic of cancer screening and prevention. A few of my ongoing research projects include: Understanding and modeling differences in breast and cervical cancer screening by sexual orientation Modeling approaches to improve Colorectal Cancer...","categories": [],
-        "tags": [],
-        "url": "/about/",
         "teaser": null
       },]
